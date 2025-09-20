94b4442e4051215c3d40d534d02f34ad468bb604
Xander Allen Rola <20190016434@my.xu.edu.ph>
2025-09-20 15:51:29 +0800

    made the parent container height 100% and centered the landing page vertically

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 // App.jsx
 import './App.css';
+import SignIn from './pages/SignIn';
 import LandingPage from './pages/LandingPage';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,15 @@
+import './SignIn.css'
+import swipeShelfLogo from '../assets/SwipeShelf.png'
+
+function SignIn() {
+  return (
+    <>
+        <div className="container">
+            <img src={swipeShelfLogo} className="logo" alt="SwipeShelf logo" />
+            
+        </div>
+    </>
+  )
+}
+
+export default SignIn;
