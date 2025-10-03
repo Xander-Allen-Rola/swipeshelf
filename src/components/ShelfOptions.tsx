baeb84a1eb81d108edb3239a6bfb4c6cdb2782fa
Xander Allen Rola <20190016434@my.xu.edu.ph>
2025-10-04 01:16:32 +0800

    added x motion for animation of shelf options

--- src/components/ShelfOptions.tsx
+++ src/components/ShelfOptions.tsx
@@ -9,9 +9,9 @@ function ShelfOptions({ onClose }: ShelfOptionsProps) {
   return (
     <motion.div
       className="shelf-options-container"
-      initial={{ opacity: 0, y: -10 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -10 }}
+      initial={{ opacity: 0, y: -10, x: -5 }}
+      animate={{ opacity: 1, y: 0, x: 0 }}
+      exit={{ opacity: 0, y: -10, x: -5 }}
       transition={{ duration: 0.2 }}
     >
       <div className="shelf-option-text">Mark as Finished</div>
